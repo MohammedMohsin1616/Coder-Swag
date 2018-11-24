1eaeaf18a63b5c73d9fe5c911c5071a185d3a9d6
Mohammed <mohammedmohsin68217@gmail.com>
2018-11-24 17:29:59 +0200

    Design Custom Collection View Cell

---
+++ Coder-Swag/View/ProductCVCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCVCell.swift
+//  Coder-Swag
+//
+//  Created by Mohammed Mohsin Sayed on 11/24/18.
+//  Copyright © 2018 Mohammed Mohsin Sayed. All rights reserved.
+//
+
+import UIKit
+
+class ProductCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
